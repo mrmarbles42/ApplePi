3935fb82fcd8b288392c3f962bb813c3aa493731
Evan <ejkrause@umass.edu>
2022-02-28 07:51:11 -0500

    Create sensorRead.ino

---
+++ arduino_files/sensorRead/sensorRead.ino
@@ -0,0 +1,26 @@
+//declare values to respective analog pins
+int humid = ;
+int lit = ;
+int temp = ;
+
+//global values
+int dataH, int dataL, int dataT;
+float sensorVal[] = {0,0,0};
+int freq = 10000; //collect data every 10 seconds
+
+void setup() {
+	//initial setup code (baud, pinmodes, etc...)
+	serial.begin(9600)
+	pinMode(humid, INPUT);
+	pinMode(lit, INPUT);
+	pinMode(temp, INPUT);
+}
+
+void loop() {
+	dataT = analogRead(temp);
+	dataH = analogRead(humid);
+	dataL = analogRead(lit);
+	
+	delay(freq);
+
+}	
